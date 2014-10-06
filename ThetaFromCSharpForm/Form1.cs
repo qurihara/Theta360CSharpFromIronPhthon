d15580956542e0d31a5dd44df3a41f8f216d1170
qurihara <qurihara@gmail.com>
2014-10-06 11:11:39 +0900

    first commit

---
+++ ThetaFromCSharpForm/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+// see these links:
+// http://st63jun.hatenablog.jp/entry/2013/07/22/001423
+// http://symfoware.blog68.fc2.com/blog-entry-416.html
+
+
+namespace ThetaFromCSharpForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ScriptEngine engine = Python.CreateEngine();
+
+            ScriptSource src = engine.CreateScriptSourceFromFile("theta360.py");
+            //ScriptSource src = engine.CreateScriptSourceFromString("print \"Hello Python World\"");
+            src.Execute();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ScriptEngine engine = IronPython.Hosting.Python.CreateEngine();
+            ScriptScope scope = engine.CreateScope();
+            ScriptSource src = engine.CreateScriptSourceFromFile("theta360.py");
+            src.Execute(scope);
+
+            Action hello = engine.Operations.GetMember<Action>(scope, "capture");
+            hello(); // => 'hello, world'
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ScriptEngine engine = IronPython.Hosting.Python.CreateEngine();
+            ScriptScope scope = engine.CreateScope();
+            ScriptSource src = engine.CreateScriptSourceFromFile("theta360.py");
+
+            object fooObject = engine.Operations.Invoke(scope.GetVariable("THETA360"));
+            Action bar = engine.Operations.GetMember<Action>(fooObject, "InitiateCapture");
+            bar(); // => 'Foo.bar()'
+
+        }
+    }
+}
