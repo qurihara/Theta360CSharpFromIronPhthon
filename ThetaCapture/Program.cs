c2958934b5abe4b0a9f2cb0c7e7b8ec2208d0719
qurihara <qurihara@gmail.com>
2014-10-16 16:54:46 +0900

    added command line app

---
+++ ThetaCapture/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThetaCapture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string saveDir = "C:/tmp/server/imgs/";
+            string saveDir = args[0];
+            Console.WriteLine(args[0]);
+            bool suc = Theta360Wrapper.Theta360.Capture(saveDir);
+
+        }
+    }
+}
